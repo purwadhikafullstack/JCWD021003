8bf89b266f1dda20f668fe45a284133cd9aa3d86
yoelwildanto <yoel.wildantop@gmail.com>
2023-12-21 16:48:17 +0700

    feat: create rest api for authentication

--- apps/api/src/routers/auth.router.js
+++ apps/api/src/routers/auth.router.js
@@ -1,5 +1,27 @@
-import {Router} from 'express'
+import { Router } from 'express'
+import { registerController, emailVerificationController, loginController, keepLoginController, forgotPasswordController, resetPasswordController } from "../controllers/auth.controller";
+import { validator } from '../middleware/validator.middleware';
+import { verifyToken } from '../middleware/auth.middleware';
+import { body } from "express-validator";
 
 const authRouter = Router();
 //GET 
-authRouter.get("keep-login", verifyToken)
+authRouter.get("keep-login", verifyToken, keepLoginController)
+
+//POST
+authRouter.post("/user-registration", validator(validations), registerController);
+authRouter.post("/login", loginController);
+authRouter.post("/request-password-reset", forgotPasswordController);
+
+// PUT
+authRouter.put("/email-verification", emailVerificationController);
+authRouter.put("/reset-password", resetPasswordController);
+
+// VALIDATION USING EXPRESS VALIDATOR
+const validations = [
+    body("email").notEmpty().withMessage("Email cannot be emptied"),
+    body("email").isEmail().withMessage("Email format is invalid"),
+    body("username").notEmpty().withMessage("Username cannot be emptied"),
+];
+
+export { authRouter };
