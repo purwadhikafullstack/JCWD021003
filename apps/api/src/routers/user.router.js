52e5319684a7950a0e1a5f3c7f14db11e0ff8631
yoelwildanto <yoel.wildantop@gmail.com>
2024-02-04 16:11:28 +0700

    feat: add middleware in user router

--- apps/api/src/routers/user.router.js
+++ apps/api/src/routers/user.router.js
@@ -1,20 +1,20 @@
 import { Router } from 'express';
 import { updateUsernameController, updateEmailController, updatePasswordController, uploadAvatarFileController,findAdminController,findUserController,updateUserController,deleteUserController } from '../controllers/user.controller';
 import { uploadAvatarFile } from '../middleware/multer.middleware';
-import { verifyToken, checkRoleSuperadmin, checkRoleAdmin } from '../middleware/auth.middleware';
+import { verifyToken, checkRoleSuperadmin } from '../middleware/auth.middleware';
 const userRouter = Router();
 
 //GET
 userRouter.get('/admin', findAdminController)
-userRouter.get('/user', findUserController)
+userRouter.get('/user', verifyToken, checkRoleSuperadmin,findUserController)
 
 //PATCH
-userRouter.patch('/:id', updateUserController)
-userRouter.patch("/update-username/:id", updateUsernameController);
-userRouter.patch("/update-email/:id", updateEmailController);
-userRouter.patch("/update-password/:id", updatePasswordController);
-userRouter.patch("/upload-avatar/:id", uploadAvatarFile, uploadAvatarFileController)
+userRouter.patch('/:id',verifyToken, checkRoleSuperadmin, updateUserController)
+userRouter.patch("/update-username/:id", verifyToken, updateUsernameController);
+userRouter.patch("/update-email/:id", verifyToken, updateEmailController);
+userRouter.patch("/update-password/:id", verifyToken, updatePasswordController);
+userRouter.patch("/upload-avatar/:id", verifyToken,  uploadAvatarFile, uploadAvatarFileController)
 
 //DELETE
-userRouter.delete('/:id',  deleteUserController)
+userRouter.delete('/:id', verifyToken, checkRoleSuperadmin, deleteUserController)
 export {userRouter};
